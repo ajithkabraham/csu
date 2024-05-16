9f5a267bccff050d126d9e6fdd5f172333082002
ajithkabraham <64701492+ajithkabraham@users.noreply.github.com>
2024-05-16 12:15:19 -0400

    initial commit for module1

---
+++ python/module1/part1.py
@@ -0,0 +1,16 @@
+# program to add two numbers
+
+# Get the first input
+num1 = int(input('Enter the first number:\n'))
+# Get the second input
+num2 = int(input('Enter the second number:\n'))
+
+# Do the sum
+sum1_2 = num1 + num2
+
+# Do the difference
+diff1_2 = num1 - num2
+
+# Print the output
+print("Sum of the two numners", num1, num2, " is ", sum1_2)
+print("Difference of the two numners", num1, num2, " is ", diff1_2)
