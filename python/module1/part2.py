6649f2a9a5a9269f89f15e4b1614ef2ace9cfa02
ajithkabraham <64701492+ajithkabraham@users.noreply.github.com>
2024-05-16 14:03:09 -0400

    initial checkin for part2

---
+++ python/module1/part2.py
@@ -0,0 +1,16 @@
+# program to multiply and divide two numbers
+
+# Get the first input
+num1 = float(input('Enter the first number:\n'))
+# Get the second input
+num2 = float(input('Enter the second number:\n'))
+
+# Do the sum
+prod1_2 = num1 * num2
+
+# Do the difference
+quot1_2 = num1 - num2
+
+# Print the output
+print("Product of the two numbers", num1, num2, " is ", prod1_2)
+print("Quotient of the two numners", num1, num2, " is ", quot1_2)
