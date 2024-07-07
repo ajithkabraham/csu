f9f9268f7788f9a6cd2be8e3d67fd51c15588fdc
ajithkabraham <64701492+ajithkabraham@users.noreply.github.com>
2024-07-06 21:07:34 -0400

    :final update to shopping cart

--- python/module4/online_shopping_cart.py
+++ python/module4/online_shopping_cart.py
@@ -1,22 +1,29 @@
+from datetime import datetime
+
 # Class Item to purchase
 class ItemToPurchase:
+    count = 0
 
     # constructor function    
-    def __init__(self, item_name = 'none', item_price = 0, item_quantity = 0 ):
-        self.item_name = item_name,
-        self.item_price = item_price,
+    def __init__(self, item_name = 'none', item_price = 0, item_quantity = 0, item_description = '' ):
+        self.item_name = item_name
+        self.item_price = item_price
         self.item_quantity = item_quantity
+        self.item_description = item_description
+        ItemToPurchase.count += 1
     # function to print the total cost
     def print_item_cost(self):
-        print(self.item_name[0],' ', self.item_quantity, " @ $", self.item_price[0], ' = ${:.2f}'.format(self.item_quantity * self.item_price[0]),sep='' )
-        return (self.item_quantity * self.item_price[0])
+        print(self.item_name,' ', self.item_quantity, " @ $", self.item_price, ' = ${:.2f}'.format(self.item_quantity * self.item_price),sep='' )
+        return (self.item_quantity * self.item_price)
 
 class ShoppingCart:
     cart_items = []
+    count = 0 
     # constructor function    
     def __init__(self, cust_name = 'none', date = 'January 1, 2020' ):
-        self.cust_name = cust_name,
+        self.cust_name = cust_name
         self.date = date
+        ShoppingCart.count += 1
     
 
     def add_item(self,itemToPurchase):
@@ -29,19 +36,29 @@ class ShoppingCart:
             print(" Item not found in cart")
 
     def modify_item(self, itemToPurchase):
-        if itemToPurchase.item_name in self.cart_items:
-            if itemToPurchase.item_price == 0 or itemToPurchase.item_quantity == 0:
-                #modify
-                itemToPurchase.item_price = 0
-            else:
-                print("Item not found in cart")
+        print('To modify quantity, press q')
+        print('To modify description, press d')
+        print('To modify price, press p\n')
+        type_to_modify = input()
+        if type_to_modify == 'q':
+            new_qty = int(input('Enter new quantity\n'))
+            itemToPurchase.item_quantity = new_qty
+        elif type_to_modify == 'd':
+            new_desc = input('Enter new description\n')
+            itemToPurchase.item_description = new_desc
+        elif type_to_modify == 'p':
+            new_price = float(input('Enter the new price\n'))
+            itemToPurchase.item_price = new_price
+            print( itemToPurchase.item_price)
+        else:
+            print("Invalid input")
     def get_num_items_in_cart(self):
         return len(self.cart_items)
     
     def get_cost_of_cart(self):
         total_cost = 0
         for item in self.cart_items:
-            total_cost = total_cost + (item.item_price[0] * item.item_quantity)
+            total_cost = total_cost + (item.item_price * item.item_quantity)
         return total_cost
     def print_total(self,itemToPurchase):
         total_cost = self.get_cost_of_cart()
@@ -53,38 +70,6 @@ class ShoppingCart:
         print(self.cust_name)
         
 # Main code
-# Gather details for first item
-print("Item 1")
-item_name = input("Enter the item name:\n")
-item_price = float(input("Enter the item price:\n"))
-item_quantity = int(input("Enter the item quantity:\n"))
-item1 = ItemToPurchase(item_name, item_price,item_quantity)
-#item1 = ItemToPurchase('Chips', 2,10)
-
-# Gather details for second item
-print("Item 2")
-item_name = input("Enter the item name:\n")
-item_price = float(input("Enter the item price:\n"))
-item_quantity = int(input("Enter the item quantity:\n"))
-item2 = ItemToPurchase(item_name, item_price,item_quantity)
-#item2 = ItemToPurchase('headphones', 3,40)
-
-# Print the item details
-print('TOTAL COST')
-item1_total = item1.print_item_cost()
-item2_total = item2.print_item_cost()
-print('Total: ${:.2f}'.format(item1_total + item2_total))
-
-shoppingCart1 = ShoppingCart('John Doe', 'June 16, 2024')
-print(shoppingCart1.cust_name)
-shoppingCart1.add_item(item1)
-shoppingCart1.add_item(item2)
-print(shoppingCart1.get_num_items_in_cart())
-#shoppingCart1.remove_item(item1)
-#print(shoppingCart1.get_num_items_in_cart())
-print(shoppingCart1.get_cost_of_cart())
-
-
 def print_menu():
     print('MENU\n')
     print('a - Add item to cart\n')
@@ -98,25 +83,60 @@ def print_menu():
     return menu_input
 def print_output():
     print('OUTPUT SHOPPING CART')
-    print(shoppingCart1.cust_name[0], '\'s Shopping Cart - ', shoppingCart1.date)
-    print('Number of Items: ',shoppingCart1.get_num_items_in_cart())
-    for item_name in shoppingCart1.cart_items:
+    print(shoppingCart.cust_name, '\'s Shopping Cart - ', shoppingCart.date)
+    print('Number of Items: ',shoppingCart.get_num_items_in_cart())
+    for item_name in shoppingCart.cart_items:
         item_name.print_item_cost()
-    print('Total: ${:.2f}'.format(shoppingCart1.get_cost_of_cart()))
+    print('Total: ${:.2f}'.format(shoppingCart.get_cost_of_cart()))
+def print_description():
+    print('OUTPUT ITEMS\' DESCRIPTIONS')
+    print(shoppingCart.cust_name, '\'s Shopping Cart - ', shoppingCart.date)
+    print('Item Descriptions')
+    for item in shoppingCart.cart_items:
+        print(item.item_name,':',item.item_description)
+items_list = []
+while True:
+    menu_input = print_menu()
+    if menu_input == 'q':
+        break
+    elif menu_input == 'a':
+        items_in_cart_count = ItemToPurchase.count
+        shopping_carts_count = ShoppingCart.count
+        if shopping_carts_count == 0:
+            s_cart_name = input("Enter your name:\n")
+            s_cart_date = datetime.now().strftime("%B %d, %Y")
+            shoppingCart = ShoppingCart(s_cart_name, s_cart_date)
+            print('Customer name:',s_cart_name)
+            print('Today\'s date:',s_cart_date)
+        item_name = input("Enter the item name:\n")
+        items_list.append(item_name)
+        item_desc = input('Enter the item description\n')
+        item_price = float(input("Enter the item price:\n"))
+        item_quantity = int(input("Enter the item quantity:\n"))
+        item = ItemToPurchase(item_name, item_price,item_quantity, item_desc)
+        shoppingCart.add_item(item)
 
+    elif menu_input == 'r':
+        item_to_remove = input("Enter the item to remove:\n")
+        if (item_to_remove in items_list):
+            for item in shoppingCart.cart_items:
+                if item.item_name == item_to_remove:
+                    shoppingCart.remove_item(item)
+        else:
+            print("Item not found in shopping cart.")
 
-menu_input = print_menu()
-if menu_input == 'q':
-    print('quit')
-elif menu_input == 'a':
-    print('add new items')
-elif menu_input == 'r':
-    print('remove items')
-elif menu_input == 'i':
-    print('get descriptions')
-elif menu_input == 'o':
-    print_output()
-else:
-    print('Please enter a valid choice from the menu\n')
-    print_menu()
+    elif menu_input == 'c':
+        item_to_modify = input('Enter item to modify\n')
+        if (item_to_modify in items_list):
+            for item in shoppingCart.cart_items:
+                if item.item_name == item_to_modify:
+                    shoppingCart.modify_item(item)
+        else:
+            print("Item not found in shopping cart.")
+    elif menu_input == 'i':
+        print_description()
+    elif menu_input == 'o':
+        print_output()
+    else:
+        print('Please enter a valid choice from the menu\n')
 
--- python/module4/test_mod.py
+++ python/module4/test_mod.py
@@ -1,6 +1,29 @@
-input_number = int(input())
-bin_num = ''
-while (input_number > 0 ):   
-    bin_num = bin_num + str(input_number % 2)
-    input_number = input_number //2
-print(bin_num)
+class Pizza:
+    # constructor function    
+    def __init__(self, pizza_size = '12', pizza_type = 'pepperoni', pizza_qty = 1 ):
+        self.pizza_size  = pizza_size,
+        self.pizza_type = pizza_type,
+        self.pizza_qty = pizza_qty
+
+    __ingred_list_cheese = ['dough','sauce','mozzarella','Cheddar', 'Havarti', 'Provolone']
+    __ingred_list_meat = ['dough','sauce','mozzarella','sausage', 'beef', 'bacon']
+    __ingred_list_pepperoni = ['dough','sauce','Provolone','pepperoni']
+
+    # function to make pizza
+    def make_pizza(self):
+        ingredients_list = self.__ingred_list_pepperoni[:]
+        if self.pizza_type[0] == 'cheese':
+            ingredients_list = self.__ingred_list_cheese[:]
+        elif self.pizza_type[0] == 'meatlovers':
+            ingredients_list = self.__ingred_list_meat[:]
+        else:
+            print("Invalid pizza")
+	
+        print('1. Flatten the dough to size,self.pizza_size')
+        print('2. Add sauce on pizza')
+        print('3. Add ingredients')
+        print("\n".join('%s'%item for item in ingredients_list))
+        print('4. Bake at 750 degrees(F) for 5 mins')
+
+cheese_pizza1 = Pizza('14','meatlovers',1)
+cheese_pizza1.make_pizza()
